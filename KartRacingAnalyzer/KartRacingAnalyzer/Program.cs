18790059959b6770a851e181c195dc5c42300584
Itiro Inazawa <itiro.inazawa@hotmail.com>
2019-03-16 16:52:18 -0300

    Application entry point

---
+++ KartRacingAnalyzer/KartRacingAnalyzer/Program.cs
@@ -0,0 +1,17 @@
+﻿using KartRacingAnalyzer.Business;
+using System;
+
+namespace KartRacingAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var file = @"C:\Users\itiro\Desktop\Job-Tests\Gympass\github\gp-it\Files\kart-racing.log";
+
+            KartBU business = new KartBU();
+            business.ExecutarCorrida(file);
+        }
+    }
+}
